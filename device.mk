02b0cdd67f00e4c62b0247bc3a5f6237824e0c5e
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-20 19:21:09 +0200

    diting: Add support for UDFPS long press sensor
    Change-Id: Ia91218bbb07c15c0dd1dcc286a8be0eae7814f75

--- device.mk
+++ device.mk
@@ -45,6 +45,13 @@ PRODUCT_PACKAGES += \
     DitingWifiOverlay \
     DitingWifiMainlineOverlay
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.xiaomi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
