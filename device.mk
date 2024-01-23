b246bb43350764d7ed59bbb33caf55c36066fa28
bheatleyyy <sugakesshaa@pixelos.net>
2024-01-23 11:07:52 +0100

    diting: Switch to aosp lift to wake impl
    - the custom doze impl is funky and doesn't work properly
    
    Change-Id: I45a31ba72fb00143fadf41b8bc503ac37f6dc901

--- device.mk
+++ device.mk
@@ -154,15 +154,6 @@ PRODUCT_VENDOR_PROPERTIES += \
     vendor.display.enable_hist_intr=1 \
     vendor.display.idle_time=0
 
-# Doze
-PRODUCT_PACKAGES += \
-    ParanoidDoze
-
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.sensor.pickup=xiaomi.sensor.pickup \
-    ro.sensor.pickup.lower.value=2 \
-    ro.sensor.proximity=true
-
 # DPM
 PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.dpm.vndr.feature=1 \
