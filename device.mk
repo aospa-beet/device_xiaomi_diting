9373b9d082d67a75f6cca1ca134cca0342df173c
bheatleyyy <sugakesshaa@pixelos.net>
2024-01-23 10:58:48 +0100

    diting: Switch to Xiaomi common sensor multihal service
    Change-Id: I6769a1250d810cc2cf6add901e72ae50c18ee14d

--- device.mk
+++ device.mk
@@ -362,8 +362,11 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.xiaomi-multihal \
     libsensorndkbridge
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
