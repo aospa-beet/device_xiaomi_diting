f7cecfe1a291f38cb0f041b6c55b1508d1d078f3
Sugakesshaa <sugakesshaa@pixelos.net>
2024-02-05 19:35:26 +0800

    diting: Move decommonised stuff from common
    Change-Id: Ie66d57681f90e741ac8c8ebee3c19d911d0e7f6f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,3 +19,12 @@ include device/xiaomi/sm8450-common/BoardConfigCommon.mk
 
 # Include proprietary files
 include vendor/xiaomi/diting/BoardConfigVendor.mk
+
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
+
+# OTA
+TARGET_OTA_ASSERT_DEVICE := diting|ditingp
+
+# Screen density
+TARGET_SCREEN_DENSITY := 480
--- device.mk
+++ device.mk
@@ -18,6 +18,19 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/diting/diting-vendor.mk)
 
+# Firmware
+$(call inherit-product-if-exists, vendor/xiaomi/firmware/diting/config.mk)
+
+# Camera
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.product.mod_device=diting_global
+
+# Characteristics
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Kernel
+KERNEL_PREBUILT_DIR := $(LOCAL_PATH)-kernel
+
 # Overlays
 PRODUCT_PACKAGES += \
     AOSPADitingFrameworksOverlay \
